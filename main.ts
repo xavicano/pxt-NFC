5a5db9ae07f9720e56aa7be41a755ba11c363d2d
xavicano <xavi.cano@ibanus.cat>
2021-01-05 09:50:47 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -140,9 +140,9 @@ namespace NFC {
         // myBuffer = [0x00, 0x00, 0xFF, 0x04, 0xFC, 0xD4, 0x4A, 0x01, 0x00, 0xE1, 0x00]
            myBuffer = [0x00, 0x00, 0xFF, 0x04, 0xFC, 0xD4, 0x40, 0x01, 0x30]
         myBuffer[9]=blockNumber
-        myBuffer[10]=myBuffer[2]           // CheckSum
+        myBuffer[10]=myBuffer[2]  +  myBuffer[5]       // CheckSum
         for (let i = 0; i < myBuffer[3]; i++) {
-            myBuffer[10]+= myBuffer[5 + i];
+            myBuffer[10]+= myBuffer[6 + i];
         } 
         myBuffer[10]=(~myBuffer[10]) & 0xFF
         //serial.writeValue("Checksum = ", checksum)
