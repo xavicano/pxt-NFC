24b27f271e9aa37aff14664d658f398792d8563a
xavicano <xavi.cano@ibanus.cat>
2021-01-05 10:26:15 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -153,11 +153,11 @@ namespace NFC {
         receivedLen = RxBufferedSize();
         //if (receivedLen > 20) {
             receivedBuffer = serial.readBuffer(25);
-            for (let i = 0; i < 16; i++) {
-                dataBuffer[i]= receivedBuffer[8 + i];
-            }
+            //for (let i = 0; i < 16; i++) {
+            //    dataBuffer[i]= receivedBuffer[8 + i];
+            //}
             //dataBuffer = [uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], uid[6],uid[7],uid[8],uid[9],uid[10],uid[11], uid[12], uid[13], uid[14], uid[15]];
-            return convertString(dataBuffer,16) ;
+            return convertString(receivedBuffer,16) ;
             //return convertString(myBuffer,12) ;
         //} else {
             //return "";
