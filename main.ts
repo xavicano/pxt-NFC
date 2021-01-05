3e72acf05168ba8c4178fabd20806ba85f5eea97
xavicano <xavi.cano@ibanus.cat>
2021-01-05 09:20:03 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -159,7 +159,8 @@ namespace NFC {
             //dataBuffer = [uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], uid[6],uid[7],uid[8],uid[9],uid[10],uid[11], uid[12], uid[13], uid[14], uid[15]];
             return convertString(dataBuffer,16) ;
         } else {
-            return "";
+            //return "";
+            return convertString(myBuffer[10],1);
         }
     }
 
